e069e51825003bead4bb74e63a28fe79425a4ff4
Mario Zoth <mario.zoth@gmail.com>
2014-10-29 10:43:31 +0100

    JS Object 'TABLA' boilerplate

--- client/js/game.js
+++ client/js/game.js
@@ -1,2 +1,12 @@
-var TABLA = {};
+var TABLA = (function () {
+    'use strict';
+
+    var tabla = {};
+
+    tabla.initialize = function () {
+        return true;
+    };
+
+    return tabla;
+}());
 
