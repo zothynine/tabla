a7f2c49637ce39f65f500d7afd9ee37519d610b4
Mario Zoth <mario.zoth@icloud.com>
2019-11-04 22:12:55 +0100

    Handle click events on cards

--- ts/index.ts
+++ ts/index.ts
@@ -1,5 +1,19 @@
 import { get as getSVGSprite } from './cards-svg-sprite.js'
 
+function getCardTablePositon(card: HTMLElement): HTMLElement {
+
+    let _tablePosition: HTMLElement;
+
+    console.log( card, card.closest('.js-pos') );
+}
+
+function onCardClickOrTab(e: Event): void {
+
+    const _TARGET = e.target
+    const _CARD = _TARGET.closest('.card')
+    getCardTablePositon(_CARD)
+}
+
 function generateRandomDeck(sprite: HTMLElement): void {
 
     const _SPRITE: HTMLElement = sprite || null
@@ -9,13 +23,13 @@ function generateRandomDeck(sprite: HTMLElement): void {
     }
 
     const _CARDS: Array<HTMLElement> = Array.prototype.slice.call(_SPRITE.querySelectorAll('svg > symbol[id*="-"]'))
+    const _DRAW_PILE: HTMLElement = document.getElementById('js-draw-pile')
 
     while (_CARDS.length > 0) {
 
         const _I: number = Math.floor(Math.random() * _CARDS.length)
         const _CARD: HTMLElement = _CARDS[_I]
         const _ID: string = _CARD.getAttribute('id')
-        const _DRAW_PILE: HTMLElement = document.getElementById('js-draw-pile')
 
         _DRAW_PILE.insertAdjacentHTML('beforeend', `
             <div class="card" id="${_ID.replace('-','')}">
@@ -28,6 +42,10 @@ function generateRandomDeck(sprite: HTMLElement): void {
 
         _CARDS.splice(_I, 1)
     }
+
+    _DRAW_PILE.querySelectorAll('.card').forEach(card => {
+        card.addEventListener('click', onCardClickOrTab)
+    })
 }
 
 window.addEventListener('message', event => {
