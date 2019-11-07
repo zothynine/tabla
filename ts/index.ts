369e3387cd3b9a973ccbc63fcda4580986609fc8
Mario Zoth <mario.zoth@icloud.com>
2019-11-07 22:39:38 +0100

    Move cards around a bit

--- ts/index.ts
+++ ts/index.ts
@@ -2,15 +2,43 @@ import { get as getSVGSprite } from './cards-svg-sprite.js'
 
 function getTableArea(card: HTMLElement): String {
 
-    let _tablePosition: HTMLElement
+    if (!card) return
     return card.closest('.js-pos').getAttribute('data-table-area')
+}
+
+function moveCard(card: HTMLElement, targetAreaName: String): void {
+
+    if (!card || !targetAreaName) return
+    const _PLACEHOLDER_STR: String = '<div></div>'
+    const _TARGET_AREA: HTMLElement = document.querySelector(`[data-table-area='${targetAreaName}']`)
+    _TARGET_AREA.insertAdjacentHTML('beforeend', _PLACEHOLDER_STR)
+    const _PLACEHOLDER: HTMLElement = _TARGET_AREA.querySelector('div:last-child')
+    const _CURRENT_POS: HTMLElement = document.querySelector(`[data-table-area='${getTableArea(card)}']`)
+    const _CURRENT_X: Number = _CURRENT_POS.getBoundingClientRect().left
+    const _CURRENT_Y: Number = _CURRENT_POS.getBoundingClientRect().top
+    const _TARGET_POS: HTMLElement = _TARGET_AREA.querySelector(`div:last-child`)
+    const _TARGET_X: Number = _TARGET_POS.getBoundingClientRect().left
+    const _TARGET_Y: Number = _TARGET_POS.getBoundingClientRect().top
+
+    document.body.insertAdjacentElement('afterbegin', card);
+    card.setAttribute('style', `top: ${_CURRENT_Y}px; left: ${_CURRENT_X}px; z-index: 1;`)
+    setTimeout(() => {
+        card.style.top = _TARGET_Y + 'px';
+        card.style.left = _TARGET_X + 'px';
+    },10),
+    card.removeEventListener('click', onCardClickOrTab)
+    setTimeout(() => {
+        _TARGET_AREA.removeChild(_PLACEHOLDER)
+        _TARGET_AREA.insertAdjacentElement('beforeend', card)
+        card.removeAttribute('style')
+    }, 1000);
 }
 
 function onCardClickOrTab(e: Event): void {
 
     const _TARGET: HTMLElement = (e.target as HTMLElement)
     const _CARD: HTMLElement = _TARGET.closest('.card')
-    console.log( getTableArea(_CARD) )
+    moveCard(_CARD, 'hand')
 }
 
 function generateRandomDeck(sprite: HTMLElement): void {
